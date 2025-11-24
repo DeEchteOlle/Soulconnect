bafe76e4d9e9679e95a2994bbec7f1d0f44a342f
DeEchteOlle <ollekuiper07@gmail.com>
2025-11-24 13:21:57 +0100

    fix the hamburger menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,19 +20,19 @@
                     </button>
                 </form>
             @else
-                <a href="{{ route('login') }}" class="text-emerald-700 hover:text-emerald-900>
+                <a href="{{ route('login') }}" class="text-emerald-700 hover:text-emerald-900">
                     Login
                 </a>
             @endauth
         </div>
 
-        <div class=" md:hidden">
-                <button id="mobile-menu-button" class="focus:outline-none">
-                    <svg class="w-7 h-7 text-gray-700" fill="none" stroke="currentColor" stroke-width="2"
-                         viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M4 6h16M4 12h16M4 18h16"/>
-                    </svg>
-                </button>
+        <div class="md:hidden">
+            <button id="mobile-menu-button" class="focus:outline-none">
+                <svg class="w-7 h-7 text-gray-700" fill="none" stroke="currentColor" stroke-width="2"
+                     viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M4 6h16M4 12h16M4 18h16"/>
+                </svg>
+            </button>
         </div>
     </div>
 
